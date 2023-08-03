9afe763dd2d396d5e4da3cad6c99f24869d4410a
wizkiye <wizkiye@gmail.com>
2023-08-03 10:41:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
+from pathlib import Path
+
 from setuptools import setup, find_packages
 
 setup(
     name="pyEthioNews",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/wizkiye/pyEthioNewsApi",
     license="MIT",
     author="https://github.com/wizkiye",
     author_email="wizkiye@gmail.com",
     description="Ethiopian News Scraper",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
     install_requires=[
         "httpx",
         "bs4",
